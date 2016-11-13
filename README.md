df1e94e178ae66cdf4772b095ef187426c377155
Jose Ignacio Acin Pozo <joseignacio.acinpozo@gmail.com>
2016-11-13 15:08:05 +0000

    RXAEROSPIKE-1 First commit and first version (0.1.0) to master branch

---
+++ README.md
@@ -0,0 +1,20 @@
+RxAerospike is a wrapper for [aerospike-client-java](https://github.com/aerospike/aerospike-client-java) which re-implements the database operations using RxJava2 and Kotlin.
+
+#### Current features
+
+* RxAerospike implements all **non-deprecated** operations from [IAsyncClient](https://github.com/aerospike/aerospike-client-java/blob/master/client/src/com/aerospike/client/async/IAsyncClient.java) and every operation returning a [Task](https://github.com/aerospike/aerospike-client-java/blob/master/client/src/com/aerospike/client/task/Task.java). 
+* Synchronous operations have not been implemented yet, but they will be. Also, certain scan operations are implemented in a strange way, which does not send onCompleted() or onFailure() notifications (although they can throw exceptions), and this increases the difficulty of wrapping them.
+* All the other operations implemented in AerospikeClient and AsyncClient are still available in this wrapper (synchronous, admin and scan operations from [IAerospikeClient](https://github.com/aerospike/aerospike-client-java/blob/master/client/src/com/aerospike/client/IAerospikeClient.java)), but not implemented using RxJava.
+
+#### Why Kotlin?
+
+* Kotlin makes it very easy to implement [class delegation](https://kotlinlang.org/docs/reference/delegation.html) by eliminating all the boilerplate code.
+* Also Kotlin supports default parameters, which generate different methods auto-magically in Java by using the annotation [@JvmOverloads](https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.jvm/-jvm-overloads/), reducing boilerplate code even more.
+#### FAQ
+**Why not implement the wrapper using Kotlin extension functions?**
+In a Kotlin world, that would be the way to go and the ideal implementation. However, this wrapper is intended to be compatible with Java applications as well, so class delegation is a better option.
+
+#### Contact and contributions
+
+I will setup a contribution model soon, please use this mailing list for any questions for now: https://groups.google.com/forum/#!forum/rxaerospike
+
---
+++ src/main/kotlin/com/github/ganet/rx/aerospike/AerospikeRxClient.kt
@@ -0,0 +1,540 @@
+/*
+ * Copyright 2016 Jose Ignacio Acin Pozo. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.ganet.rx.aerospike
+
+
+import com.aerospike.client.AerospikeException
+import com.aerospike.client.BatchRead
+import com.aerospike.client.Bin
+import com.aerospike.client.Key
+import com.aerospike.client.Language
+import com.aerospike.client.Operation
+import com.aerospike.client.Record
+import com.aerospike.client.Value
+import com.aerospike.client.async.AsyncClient
+import com.aerospike.client.async.IAsyncClient
+import com.aerospike.client.listener.BatchListListener
+import com.aerospike.client.listener.BatchSequenceListener
+import com.aerospike.client.listener.DeleteListener
+import com.aerospike.client.listener.ExecuteListener
+import com.aerospike.client.listener.ExistsArrayListener
+import com.aerospike.client.listener.ExistsListener
+import com.aerospike.client.listener.ExistsSequenceListener
+import com.aerospike.client.listener.RecordArrayListener
+import com.aerospike.client.listener.RecordListener
+import com.aerospike.client.listener.RecordSequenceListener
+import com.aerospike.client.listener.WriteListener
+import com.aerospike.client.policy.BatchPolicy
+import com.aerospike.client.policy.Policy
+import com.aerospike.client.policy.QueryPolicy
+import com.aerospike.client.policy.ScanPolicy
+import com.aerospike.client.policy.WritePolicy
+import com.aerospike.client.query.IndexCollectionType
+import com.aerospike.client.query.IndexType
+import com.aerospike.client.query.Statement
+import io.reactivex.BackpressureStrategy
+import io.reactivex.BackpressureStrategy.MISSING
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import io.reactivex.Single
+
+/**
+ * Created by JoseIgnacio on 09/11/2016.
+ */
+class AerospikeRxClient(val client: AsyncClient) : IAsyncClient by client {
+
+    /* AsyncClient Async Operations */
+    fun rxAsyncPut(policy: WritePolicy?, key: Key, vararg bins: Bin): Single<Key> {
+        return Single.create {
+            client.put(policy, object : WriteListener {
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+
+                override fun onSuccess(key: Key?) {
+                    it.onSuccess(key)
+                }
+
+            }, key, *bins)
+        }
+    }
+
+    fun rxAsyncAppend(policy: WritePolicy?, key: Key, vararg bins: Bin): Single<Key> {
+        return Single.create {
+            client.append(policy, object : WriteListener {
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+
+                override fun onSuccess(key: Key?) {
+                    it.onSuccess(key)
+                }
+
+            }, key, *bins)
+        }
+    }
+
+    fun rxAsyncPrepend(policy: WritePolicy?, key: Key, vararg bins: Bin): Single<Key> {
+        return Single.create {
+            client.prepend(policy, object : WriteListener {
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+
+                override fun onSuccess(key: Key?) {
+                    it.onSuccess(key)
+                }
+
+            }, key, *bins)
+        }
+    }
+
+    fun rxAsyncAdd(policy: WritePolicy?, key: Key, vararg bins: Bin): Single<Key> {
+        return Single.create {
+            client.add(policy, object : WriteListener {
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+
+                override fun onSuccess(key: Key?) {
+                    it.onSuccess(key)
+                }
+
+            }, key, *bins)
+        }
+    }
+
+    fun rxAsyncDelete(policy: WritePolicy?, key: Key): Single<Pair<Key, Boolean>> {
+        return Single.create {
+            client.delete(policy, object : DeleteListener {
+                override fun onSuccess(key: Key, success: Boolean) {
+                    // Nice wordplay :)
+                    it.onSuccess(key to success)
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+
+            }, key)
+        }
+    }
+
+    fun rxAsyncTouch(policy: WritePolicy?, key: Key): Single<Key> {
+        return Single.create {
+            client.touch(policy, object : WriteListener {
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+
+                override fun onSuccess(key: Key?) {
+                    it.onSuccess(key)
+                }
+            }, key)
+        }
+    }
+
+    fun rxAsyncExists(policy: Policy?, key: Key): Single<Pair<Key, Boolean>> {
+        return Single.create {
+            client.exists(policy, object : ExistsListener {
+                override fun onSuccess(key: Key, success: Boolean) {
+                    // Nice wordplay :)!
+                    it.onSuccess(key to success)
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, key)
+        }
+    }
+
+    fun rxAsyncExists(policy: BatchPolicy?, keys: Array<Key>): Single<Pair<Array<Key>, BooleanArray>> {
+        return Single.create {
+            client.exists(policy, object : ExistsArrayListener {
+                override fun onSuccess(keyArray: Array<Key>, booleanArray: BooleanArray) {
+                    it.onSuccess(keyArray to booleanArray)
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, keys)
+        }
+    }
+
+    @JvmOverloads
+    fun rxAsyncExistsSequence(policy: BatchPolicy?, keys: Array<Key>, backPressure: BackpressureStrategy = MISSING): Flowable<Pair<Key, Boolean>> {
+        return Flowable.create({
+            client.exists(policy, object : ExistsSequenceListener {
+                override fun onSuccess() {
+                    it.onComplete()
+                }
+
+                override fun onExists(key: Key, exist: Boolean) {
+                    it.onNext(key to exist)
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, keys)
+        }, backPressure)
+    }
+
+    fun rxAsyncGet(policy: Policy?, key: Key): Single<Pair<Key, Record?>> {
+        return Single.create {
+            client.get(policy, object : RecordListener {
+                override fun onSuccess(key: Key, record: Record?) {
+                    it.onSuccess(key to record)
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, key)
+        }
+    }
+
+    fun rxAsyncGet(policy: Policy?, key: Key, vararg binNames: String): Single<Pair<Key, Record?>> {
+        return Single.create {
+            client.get(policy, object : RecordListener {
+                override fun onSuccess(key: Key, record: Record?) {
+                    it.onSuccess(key to record)
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, key, *binNames)
+        }
+    }
+
+    fun rxAsyncGetHeader(policy: Policy?, key: Key): Single<Pair<Key, Record?>> {
+        return Single.create {
+            client.getHeader(policy, object : RecordListener {
+                override fun onSuccess(key: Key, record: Record?) {
+                    it.onSuccess(key to record)
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, key)
+        }
+    }
+
+    fun rxAsyncGet(policy: BatchPolicy?, records: List<BatchRead>): Single<List<BatchRead>> {
+        return Single.create {
+            client.get(policy, object : BatchListListener {
+                override fun onSuccess(list: MutableList<BatchRead>?) {
+                    it.onSuccess(list)
+                }
+
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, records)
+        }
+    }
+
+    @JvmOverloads
+    fun rxAsyncGetSequence(policy: BatchPolicy?, records: List<BatchRead>, backPressure: BackpressureStrategy = MISSING): Flowable<BatchRead> {
+        return Flowable.create({
+            client.get(policy, object : BatchSequenceListener {
+                override fun onRecord(read: BatchRead?) {
+                    it.onNext(read)
+                }
+
+                override fun onSuccess() {
+                    it.onComplete()
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, records)
+        }, backPressure)
+    }
+
+    fun rxAsyncGet(policy: BatchPolicy?, keys: Array<Key>): Single<Pair<Array<Key>, Array<Record?>>> {
+        return Single.create {
+            client.get(policy, object : RecordArrayListener {
+                override fun onSuccess(keyArray: Array<Key>, records: Array<Record?>) {
+                    it.onSuccess(keyArray to records)
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, keys)
+        }
+    }
+
+    @JvmOverloads
+    fun rxAsyncGetSequence(policy: BatchPolicy?, keys: Array<Key>, backPressure: BackpressureStrategy = MISSING): Flowable<Pair<Key, Record?>> {
+        return Flowable.create({
+            client.get(policy, object : RecordSequenceListener {
+                override fun onRecord(key: Key, record: Record?) {
+                    it.onNext(key to record)
+                }
+
+                override fun onSuccess() {
+                    it.onComplete()
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, keys)
+        }, backPressure)
+    }
+
+    fun rxAsyncGet(policy: BatchPolicy?, keys: Array<Key>, vararg binNames: String): Single<Pair<Array<Key>, Array<Record?>>> {
+        return Single.create {
+            client.get(policy, object : RecordArrayListener {
+                override fun onSuccess(keyArray: Array<Key>, records: Array<Record?>) {
+                    it.onSuccess(keyArray to records)
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, keys, *binNames)
+        }
+    }
+
+    @JvmOverloads
+    fun rxAsyncGetSequence(policy: BatchPolicy?, keys: Array<Key>, vararg binNames: String, backPressure: BackpressureStrategy = MISSING): Flowable<Pair<Key, Record?>> {
+        return Flowable.create({
+            client.get(policy, object : RecordSequenceListener {
+                override fun onRecord(key: Key, record: Record?) {
+                    it.onNext(key to record)
+                }
+
+                override fun onSuccess() {
+                    it.onComplete()
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, keys, *binNames)
+        }, backPressure)
+    }
+
+    fun rxAsyncGetHeader(policy: BatchPolicy?, keys: Array<Key>): Single<Pair<Array<Key>, Array<Record?>>> {
+        return Single.create {
+            client.getHeader(policy, object : RecordArrayListener {
+                override fun onSuccess(keyArray: Array<Key>, records: Array<Record?>) {
+                    it.onSuccess(keyArray to records)
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, keys)
+        }
+    }
+
+    @JvmOverloads
+    fun rxAsyncGetHeader(policy: BatchPolicy?, keys: Array<Key>, backPressure: BackpressureStrategy = MISSING): Flowable<Pair<Key, Record?>> {
+        return Flowable.create({
+            client.getHeader(policy, object : RecordSequenceListener {
+                override fun onRecord(key: Key, record: Record?) {
+                    it.onNext(key to record)
+                }
+
+                override fun onSuccess() {
+                    it.onComplete()
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, keys)
+        }, backPressure)
+    }
+
+
+    fun rxAsyncOperate(policy: WritePolicy?, key: Key, vararg operations: Operation): Single<Pair<Key, Record?>> {
+        return Single.create {
+            client.operate(policy, object : RecordListener {
+                override fun onSuccess(key: Key, record: Record?) {
+                    it.onSuccess(key to record)
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, key, *operations)
+        }
+    }
+
+    @JvmOverloads
+    fun rxAsyncScanAll(policy: ScanPolicy?, namespace: String, setName: String, vararg binNames: String, backPressure: BackpressureStrategy = MISSING): Flowable<Pair<Key, Record?>> {
+        return Flowable.create({
+            client.scanAll(policy, object : RecordSequenceListener {
+                override fun onRecord(key: Key, record: Record?) {
+                    it.onNext(key to record)
+                }
+
+                override fun onSuccess() {
+                    it.onComplete()
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, namespace, setName, *binNames)
+        }, backPressure)
+    }
+
+    fun rxAsyncExecute(policy: WritePolicy?, key: Key, packageName: String, functionName: String, vararg functionArgs: Value): Single<Pair<Key, Any?>> {
+        return Single.create {
+            client.execute(policy, object : ExecuteListener {
+                override fun onSuccess(key: Key, obj: Any?) {
+                    it.onSuccess(key to obj)
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, key, packageName, functionName, *functionArgs)
+        }
+    }
+
+    @JvmOverloads
+    fun rxAsyncQuery(policy: QueryPolicy?, statement: Statement, backPressure: BackpressureStrategy = MISSING): Flowable<Pair<Key, Record?>> {
+        return Flowable.create({
+            client.query(policy, object : RecordSequenceListener {
+                override fun onRecord(key: Key, record: Record?) {
+                    it.onNext(key to record)
+                }
+
+                override fun onSuccess() {
+                    it.onComplete()
+                }
+
+                override fun onFailure(exception: AerospikeException?) {
+                    it.onError(exception)
+                }
+            }, statement)
+        }, backPressure)
+    }
+    /* End of Async Operations */
+
+    /* Register/Create/Execute operations. This operations need to be polled, so ideally you should observe on background threads. */
+
+    @JvmOverloads
+    fun rxRegister(policy: Policy?, clientPath: String, serverPath: String, language: Language, sleepInterval: Int = 1000): Completable {
+        return Completable.create {
+            try {
+                client.register(policy, clientPath, serverPath, language).waitTillComplete(sleepInterval)
+                it.onComplete()
+            } catch(e: RuntimeException) {
+                it.onError(e)
+            }
+        }
+    }
+
+    @JvmOverloads
+    fun rxRegister(policy: Policy?, resourceLoader: ClassLoader, resourcePath: String, serverPath: String, language: Language, sleepInterval: Int = 1000): Completable {
+        return Completable.create {
+            try {
+                client.register(policy, resourceLoader, resourcePath, serverPath, language).waitTillComplete(sleepInterval)
+                it.onComplete()
+            } catch(e: RuntimeException) {
+                it.onError(e)
+            }
+        }
+    }
+
+    @JvmOverloads
+    fun rxRegisterUdfString(policy: Policy?, code: String, serverPath: String, language: Language, sleepInterval: Int = 1000): Completable {
+        return Completable.create {
+            try {
+                client.registerUdfString(policy, code, serverPath, language).waitTillComplete(sleepInterval)
+                it.onComplete()
+            } catch(e: RuntimeException) {
+                it.onError(e)
+            }
+        }
+    }
+
+    @JvmOverloads
+    fun rxExecute(
+            policy: WritePolicy?,
+            statement: Statement,
+            packageName: String,
+            functionName: String,
+            sleepInterval: Int = 1000,
+            vararg functionArgs: Value): Completable {
+        return Completable.create {
+            try {
+                client.execute(policy, statement, packageName, functionName, *functionArgs).waitTillComplete(sleepInterval)
+                it.onComplete()
+            } catch(e: RuntimeException) {
+                it.onError(e)
+            }
+        }
+
+    }
+
+    @JvmOverloads
+    fun rxCreateIndex(
+            policy: Policy?,
+            namespace: String,
+            setName: String?,
+            indexName: String,
+            binName: String,
+            indexType: IndexType,
+            sleepInterval: Int = 1000): Completable {
+        return Completable.create {
+            try {
+                client.createIndex(policy, namespace, setName, indexName, binName, indexType).waitTillComplete(sleepInterval)
+                it.onComplete()
+            } catch(e: RuntimeException) {
+                it.onError(e)
+            }
+        }
+
+    }
+
+    @JvmOverloads
+    fun rxCreateIndex(
+            policy: Policy?,
+            namespace: String,
+            setName: String?,
+            indexName: String,
+            binName: String,
+            indexType: IndexType,
+            indexCollectionType: IndexCollectionType,
+            sleepInterval: Int = 1000): Completable {
+        return Completable.create {
+            try {
+                client.createIndex(policy, namespace, setName, indexName, binName, indexType, indexCollectionType).waitTillComplete(sleepInterval)
+                it.onComplete()
+            } catch(e: RuntimeException) {
+                it.onError(e)
+            }
+        }
+    }
+
+    /* End of Register/Create/Execute operations */
+}
